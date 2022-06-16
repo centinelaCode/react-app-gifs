e24f27074c04dd8c35144dad1d1ab555cdc892b0
centinela1024tb <centinela1024tb@gmail.com>
2022-06-16 12:26:07 -0500

    [ADD] Se implementa el metodo para agregar una nueva category al State categories (se hace en duro)

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 
 export const GifExpertApp = () => {
 
+  // States
   const [categories, setCategories] = useState(['One punch', 'Dragon Ball']);
 
-  console.log(categories);
+  const onAddCategory = () => {
+    setCategories( [ "Samurai X", ...categories ] );
+    // setCategories( cat => [...cat, "Valorant"] );  // por medio del callback de useState
+  }
 
   return (
     <>
@@ -14,6 +18,7 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Listado de Gif (Grid de Gifs) */}
+      <button onClick={ onAddCategory } >Agregar</button>
       <ol>
         { categories.map(category => (
           <li key={ category }>{ category }</li>
