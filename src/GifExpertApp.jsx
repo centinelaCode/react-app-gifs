f7f293d275b6b7490b389a7fd5ea2352c6ed667b
centinela1024tb <centinela1024tb@gmail.com>
2022-06-16 12:15:39 -0500

    [ADD] Se agrega el state para las categories y se listan

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,25 @@
+import { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'Dragon Ball']);
+
+  console.log(categories);
+
   return (
     <>
+      {/* Title */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif (Grid de Gifs) */}
+      <ol>
+        { categories.map(category => (
+          <li key={ category }>{ category }</li>
+        )) }
+      </ol>
+        {/* GifItem */}
     </>
   )
 }
