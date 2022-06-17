458efe180a93ea977f42637a306949aa21971470
centinela1024tb <centinela1024tb@gmail.com>
2022-06-17 13:41:21 -0500

    [ADD] Se refactoriza un poco para tener en un componente idependiente cada card con su gif y title
    -> Se le pasa la image expandida por medio del spread operator para que todas las propiedas se pasen como props
    -> Y se renderizan las imagenes, ,mostrando 10 por categoria

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,22 +1,42 @@
 
 // llamada a la API por fuera del copponente
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
-
+import { GifItem } from './GifItem';
 
 
 export const GifGrid = ({ category }) => {
-  
-  // const [first, setfirst] = useState(second)
 
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    // console.log(images)
+  }
+    
   useEffect(() => {
-    getGifs(category);
+    getImages();
   },[])
 
  
   return (
     <>
       <h3>{ category }</h3>
+
+      <div className="card-grid">
+        {
+          // { ...image } significa exparcir todas las propiedades de image que son: title|id|url
+          // para que todas las propiedas se pasen como props
+          images.map( ( image ) => (                                             
+            <GifItem 
+              key={image.id}
+              { ...image }
+
+            />
+          ))
+        }
+      </div>
      
     </>
   )
