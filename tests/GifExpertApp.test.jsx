e90e3aec6932a94333612589e76a3ec5b5f2bace
centinela1024tb <centinela1024tb@gmail.com>
2022-06-23 16:27:50 -0500

    [ADD] Test al GifExpertApp => Pendiente los test

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen, renderHook, waitFor } from '@testing-library/react';
+import { GifExpertApp } from '../src/GifExpertApp';
+
+describe('Pruebas en <GifExpertApp />', () => {
+
+  test('xx', () => {
+    
+     render( <GifExpertApp /> ); 
+     screen.debug();
+
+	})
+
+
+  
+});
